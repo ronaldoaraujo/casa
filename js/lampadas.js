c7f153595c9719f0ac15367894a0072ab69c579e
Ronaldo de Sousa Araujo <ronaldo.araujo@codeminer42.com>
2019-01-16 23:58:46 -0200

    Add new buttons

--- js/lampadas.js
+++ js/lampadas.js
@@ -1,21 +1,19 @@
 this.Lampada = (function () {
   function Lampada() {
-    this.switch = function (lampada) {
-      fetch(`http://192.168.0.2/${lampada}`);
-    }
     this.load = function () {
       fetch('http://192.168.0.2')
-      .then(function(result){
-        result.json()
-        .then(function(json){
-          var elemento;
-          for(var k in json) {
-            elemento = document.getElementById(k);
-            if (elemento) {
-              elemento.checked = json[k];
-            }
+      .then(result => result.json())
+      .then(function(json) {
+        var elemento;
+        for(var k in json) {
+          elemento = document.getElementById(k);
+          if (elemento) {
+            elemento.addEventListener("click", function(){
+              fetch(`http://192.168.0.2/${this.id}`);
+            }, true);
+            elemento.checked = json[k];
           }
-        })
+        }
       })
     }
     this.load();
